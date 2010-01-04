b39ef9ae41f34fb0126ebed506d1dccd5c342deb
sandhudavis <sandhudavis@5d4eee3c-ef54-11de-88d6-afc75ad17f39>
2010-01-04 14:28:24 +0000

    new layout

--- dsm2-grid-map/src/gov/ca/bdo/modeling/dsm2/map/client/HeaderPanel.java
+++ dsm2-grid-map/src/gov/ca/bdo/modeling/dsm2/map/client/HeaderPanel.java
@@ -5,26 +5,28 @@ import gov.ca.bdo.modeling.dsm2.map.client.service.LoginService;
 import gov.ca.bdo.modeling.dsm2.map.client.service.LoginServiceAsync;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Style.Unit;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.DockLayoutPanel;
 import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.HorizontalPanel;
 
 public class HeaderPanel extends Composite {
 	private final LoginServiceAsync loginService;
 
 	public HeaderPanel() {
 		loginService = GWT.create(LoginService.class);
-		final HorizontalPanel panel = new HorizontalPanel();
+		final DockLayoutPanel panel = new DockLayoutPanel(Unit.EM);
 		panel.setStyleName("headerPanel");
-		panel.add(new HTML("<h3>DSM2 Grid Map</h3>"));
+		panel.addWest(new HTML("<h3>DSM2 Grid Map</h3>"), 40);
 		loginService.login(GWT.getHostPageBaseURL(),
 				new AsyncCallback<LoginInfo>() {
 
 					public void onSuccess(LoginInfo result) {
 						if (result.isLoggedIn()) {
-							panel.add(new Anchor(result.getEmailAddress()));
+							panel.add(new Anchor("Signed in as "
+									+ result.getEmailAddress()));
 						} else {
 							panel
 									.add(new Anchor("Login", result
