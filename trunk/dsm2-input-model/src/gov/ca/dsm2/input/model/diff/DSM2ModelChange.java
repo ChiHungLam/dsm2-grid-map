02e3383a59950af6135ad728bab68d969e03b60c
sandhudavis <sandhudavis@5d4eee3c-ef54-11de-88d6-afc75ad17f39>
2010-01-15 00:18:12 +0000

    starting work on diff model

--- trunk/dsm2-input-model/src/gov/ca/dsm2/input/model/diff/DSM2ModelChange.java
+++ trunk/dsm2-input-model/src/gov/ca/dsm2/input/model/diff/DSM2ModelChange.java
@@ -1,11 +1,27 @@
 package gov.ca.dsm2.input.model.diff;
 
+import gov.ca.dsm2.input.model.Channel;
+import gov.ca.dsm2.input.model.DSM2Model;
+
 /**
- * A change model for DSM2 Model
+ * A structure to represent to change in a model.
+ * 
+ * This can be generically represented as a tuple of (Model[Base], Model[New])
+ * where the tuple is present for all elements that are present in both models
+ * but are different in their intrinsic properties.
  * 
  * @author nsandhu
  * 
  */
 public class DSM2ModelChange {
+	private final DSM2Model modelBase;
+	private final DSM2Model modelChanged;
 
+	public DSM2ModelChange() {
+		modelBase = new DSM2Model();
+		modelChanged = new DSM2Model();
+	}
+
+	public void addChannelChange(Channel channelBase, Channel channelChanged) {
+	}
 }
