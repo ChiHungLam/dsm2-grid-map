ef52dbcd0e1bc0a2f30d424ea468bb950c309df8
sandhudavis <sandhudavis@5d4eee3c-ef54-11de-88d6-afc75ad17f39>
2010-01-27 00:27:51 +0000

    added license GPL v3 to source

--- dsm2-bathymetry-map/src/gov/ca/maps/bathymetry/tiles/server/GenericDAOImpl.java
+++ dsm2-bathymetry-map/src/gov/ca/maps/bathymetry/tiles/server/GenericDAOImpl.java
@@ -1,3 +1,21 @@
+/**
+ *    Copyright (C) 2009, 2010 
+ *    State of California,
+ *    Department of Water Resources.
+ *    This file is part of DSM2 Grid Map
+ *    The DSM2 Grid Map is free software: 
+ *    you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *    DSM2 Grid Map is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details. [http://www.gnu.org/licenses]
+ *    
+ *    @author Nicky Sandhu
+ *    
+ */
 package gov.ca.maps.bathymetry.tiles.server;
 
 import java.io.Serializable;
@@ -6,8 +24,8 @@ import java.lang.reflect.ParameterizedType;
 import javax.jdo.PersistenceManager;
 
 public class GenericDAOImpl<T> implements GenericDAO<T> {
-	private Class<T> persistentClass;
-	private PersistenceManager persistenceManager;
+	private final Class<T> persistentClass;
+	private final PersistenceManager persistenceManager;
 
 	@SuppressWarnings("unchecked")
 	public GenericDAOImpl(PersistenceManager pm) {
