1deea6810e32ec946ae6bf84219dfdaf11b5b0ac
sandhudavis <sandhudavis@5d4eee3c-ef54-11de-88d6-afc75ad17f39>
2010-05-17 22:57:45 +0000

    added guice annotations added saveObject method

---
+++ trunk/dsm2-widgets/src/gov/ca/modeling/server/utils/GenericDAOImpl.java
@@ -0,0 +1,56 @@
+/**
+ *   Copyright (C) 2009, 2010 
+ *    Nicky Sandhu
+ *    State of California,
+ *    Department of Water Resources.
+ *    This file is part of DSM2 Grid Map
+ *    The DSM2 Grid Map is free software: 
+ *    you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *    DSM2 Grid Map is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+
+ *    You should have received a copy of the GNU General Public License
+ *    along with DSM2 Grid Map.  If not, see <http://www.gnu.org/licenses>.
+ */
+package gov.ca.modeling.server.utils;
+
+import com.googlecode.objectify.Objectify;
+import com.googlecode.objectify.ObjectifyFactory;
+
+public class GenericDAOImpl<T> implements GenericDAO<T> {
+
+	protected ObjectifyFactory factory;
+
+	public GenericDAOImpl(ObjectifyFactory factory) {
+		this.factory = factory;
+	}
+
+	public T createObject(T object) {
+		Objectify ofy = factory.begin();
+		ofy.put(object);
+		return object;
+	}
+
+	public void deleteObject(T object) {
+		Objectify ofy = factory.begin();
+		ofy.delete(object);
+	}
+	
+	public void saveObject(T object){
+		Objectify ofy = factory.begin();
+		ofy.put(object);
+	}
+
+	@Override
+	public T findObjectById(Class<T> actualClass, long id) {
+		Objectify ofy = factory.begin();
+		T result = ofy.find(actualClass, id);
+		return result;
+	}
+
+}
