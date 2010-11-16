0fbfed4a961b19985c8ccd4ab497ba8bb3ffd2b2
psandhu@water.ca.gov <psandhu@water.ca.gov@5d4eee3c-ef54-11de-88d6-afc75ad17f39>
2010-11-16 20:42:16 +0000

    fixed to upload from encoded values

--- trunk/dsm2-bathymetry-processor/src/gov/ca/maps/bathymetry/processor/uploader/DEMUploader.java
+++ trunk/dsm2-bathymetry-processor/src/gov/ca/maps/bathymetry/processor/uploader/DEMUploader.java
@@ -1,10 +1,13 @@
 package gov.ca.maps.bathymetry.processor.uploader;
 
 import java.io.File;
-import java.io.FilenameFilter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.PrintWriter;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
@@ -13,68 +16,77 @@ public class DEMUploader {
 	// "http://dsm2bathymetry.appspot.com/upload_bathymetry.html";
 
 	static String baseUrl = "http://localhost:8888/upload_dem.html";
+	private int uploadEntityLimit;
+	private int lastUploadedRow;
 
-	private DEMUploader() {
+	private DEMUploader(int limit) {
+		uploadEntityLimit = limit;
+		lastUploadedRow = 0;
 	}
 
-	public boolean upload(String directory) {
+	public boolean upload(String csvFile) throws IOException {
+		boolean hasMoreLines = false;
 		HtmlUnitDriver driver = new HtmlUnitDriver();
 		driver.get(baseUrl);
-		File dir = new File(directory);
 		long ti = System.currentTimeMillis();
 		long ct = ti;
-		if (dir.exists() && dir.isDirectory()) {
-			String[] files = dir.list(new FilenameFilter() {
-
-				public boolean accept(File dir, String name) {
-					return name.endsWith(".csv");
-				}
-			});
-			if ((files == null) || (files.length == 0)) {
-				return false;
-			}
-			for (int i = 0; i < files.length; i++) {
-				if (System.currentTimeMillis() - ct > 60000) {
-					System.out.println("Uploaded " + (i + 1) + " files ...");
-					ct = System.currentTimeMillis();
-				}
-				String name = files[i];
-				String filePath = uploadDemFile(driver, dir, name);
-				if (i == 0) {
-					doLogin(driver);
-					driver.get(baseUrl);
-					uploadDemFile(driver, dir, name);
-				}
-				File processedFile = new File(filePath);
-				processedFile.renameTo(new File(filePath + ".done"));
-			}
-			System.out.println("Uploaded " + (files.length) + " files in "
-					+ ((System.currentTimeMillis() - ti) / 1000) + " seconds");
+		LineNumberReader lnr = new LineNumberReader(new FileReader(csvFile));
+		String header = lnr.readLine();
+		for (int i = 0; i < lastUploadedRow; i++) {
+			lnr.readLine();
 		}
-		return false;
+		String line = null;
+		File cFile = new File(csvFile);
+		String dir = cFile.getParent();
+		String uploadFile = dir + "/current_upload_dem.csv";
+		PrintWriter pw = new PrintWriter(uploadFile);
+		pw.println(header);
+		int count = 0;
+		while ((line = lnr.readLine()) != null) {
+			pw.println(line);
+			count++;
+			if (count >= uploadEntityLimit) {
+				hasMoreLines = true;
+				break;
+			}
+		}
+		pw.close();
+		lnr.close();
+		uploadDemFile(driver, uploadFile);
+		try {
+			WebElement emailElement = driver.findElement(By.name("email"));
+			if (emailElement != null) {
+				doLogin(driver);
+				driver.get(baseUrl);
+				uploadDemFile(driver, uploadFile);
+			}
+		} catch (NoSuchElementException ex) {
+			// do nothing
+		}
+		lastUploadedRow += count;
+		System.out.println("Uploaded " + lastUploadedRow + " in "
+				+ ((System.currentTimeMillis() - ti) / 1000) + " seconds");
+		return hasMoreLines;
 	}
 
-	private String uploadDemFile(HtmlUnitDriver driver, File dir, String name) {
-		System.out.println("Processing file: " + name);
-		String filePath = dir + "/" + name;
+	private void uploadDemFile(HtmlUnitDriver driver, String filePath) {
 		WebElement fileElement = driver.findElement(By.name("demFile"));
 		fileElement.sendKeys(filePath);
 		driver.findElement(By.xpath("//input[@value=\"Send\"]")).click();
-		return filePath;
 	}
 
 	public static void main(String[] args) throws IOException,
 			InterruptedException {
 		if (args.length < 1) {
-			System.out.println("Usage DEMUploader <directory>");
+			System.out.println("Usage DEMUploader <encoded_csv_file>");
 			System.exit(1);
 		}
-		String splitterDir = args[0];
-		DEMUploader uploader = new DEMUploader();
+		String encodedFile = args[0];
+		DEMUploader uploader = new DEMUploader(1000);
 		boolean moreFiles = true;
 		while (moreFiles) {
 			try {
-				moreFiles = uploader.upload(splitterDir);
+				moreFiles = uploader.upload(encodedFile);
 			} catch (Exception ex) {
 				ex.printStackTrace();
 			}
