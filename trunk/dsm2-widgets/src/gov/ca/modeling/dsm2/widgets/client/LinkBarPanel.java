607d6997d7bc36eca4e07fac9b9eb95daf5b338d
psandhu@water.ca.gov <psandhu@water.ca.gov@5d4eee3c-ef54-11de-88d6-afc75ad17f39>
2010-12-27 22:48:17 +0000

    styling changes

--- trunk/dsm2-widgets/src/gov/ca/modeling/dsm2/widgets/client/LinkBarPanel.java
+++ trunk/dsm2-widgets/src/gov/ca/modeling/dsm2/widgets/client/LinkBarPanel.java
@@ -1,25 +1,34 @@
 package gov.ca.modeling.dsm2.widgets.client;
 
+import com.google.gwt.event.dom.client.FocusEvent;
+import com.google.gwt.event.dom.client.FocusHandler;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
 
-public class LinkBarPanel extends Composite{
+public class LinkBarPanel extends Composite {
 
 	private FlowPanel mainPanel;
 
-	public LinkBarPanel(String stylename){
+	public LinkBarPanel(String stylename) {
 		mainPanel = new FlowPanel();
 		mainPanel.setStyleName(stylename);
 		initWidget(mainPanel);
 	}
-	
-	public void add(Anchor anchor){
+
+	public void add(final Anchor anchor) {
 		anchor.setStyleName("tab");
+		anchor.addFocusHandler(new FocusHandler() {
+
+			@Override
+			public void onFocus(FocusEvent event) {
+				anchor.setFocus(false);
+			}
+		});
 		mainPanel.add(anchor);
 	}
-	
-	public void setActiveLink(String href){
+
+	public void setActiveLink(String href) {
 		int widgetCount = mainPanel.getWidgetCount();
 		for (int i = 0; i < widgetCount; i++) {
 			Anchor anchor = (Anchor) mainPanel.getWidget(i);
