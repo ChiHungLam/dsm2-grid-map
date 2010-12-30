833887a48e9c7185e94db98da06ac91620e97e24
sandhudavis <sandhudavis@5d4eee3c-ef54-11de-88d6-afc75ad17f39>
2010-12-30 06:06:56 +0000

    work started on coloring channel lines by value

--- trunk/dsm2-grid-map/src/gov/ca/bdo/modeling/dsm2/map/client/display/ColorSchemeDisplay.java
+++ trunk/dsm2-grid-map/src/gov/ca/bdo/modeling/dsm2/map/client/display/ColorSchemeDisplay.java
@@ -1,16 +1,27 @@
 package gov.ca.bdo.modeling.dsm2.map.client.display;
 
+import gov.ca.bdo.modeling.dsm2.map.client.map.ChannelLineDataManager;
+
+import java.util.HashMap;
+
 import com.google.gwt.event.shared.EventBus;
+import com.google.gwt.maps.client.overlay.PolyStyleOptions;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class ColorSchemeDisplay extends MapDisplay {
 
+	public static interface ColorMapper {
+		public String convertValueToColor(double value);
+	}
+	
 	private FlowPanel controlPanel;
 	private FlowPanel infoPanel;
 
 	public ColorSchemeDisplay(ContainerDisplay display, EventBus eventBus) {
 		super(display, true, eventBus);
+		mapPanel.getGridVisibility().getHideNodes().setValue(true);
+		mapPanel.getGridVisibility().getHideReservoirs().setValue(true);
 		infoPanel = new FlowPanel();
 	}
 
@@ -27,6 +38,17 @@ public class ColorSchemeDisplay extends MapDisplay {
 			controlPanel = new FlowPanel();
 		}
 		return controlPanel;
+	}
+
+	public void colorizeChannels(HashMap<String, Double> channelIdToValueMap,
+			ColorMapper colorMapper) {
+		ChannelLineDataManager channelManager = mapPanel.getChannelManager();
+		for (String id : channelIdToValueMap.keySet()) {
+			Double value = channelIdToValueMap.get(id);
+			String colorSpec = colorMapper.convertValueToColor(value);
+			channelManager.getPolyline(id).setStrokeStyle(
+					PolyStyleOptions.newInstance(colorSpec));
+		}
 	}
 
 }
