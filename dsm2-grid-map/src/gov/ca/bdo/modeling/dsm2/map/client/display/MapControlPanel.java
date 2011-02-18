c566e1dff1e99133dc81c83b1ee61ccb296439ba
psandhu <psandhu@water.ca.gov>
2011-02-18 17:55:24 +0000

    add channel colorization by mannings and dispersion feature to tools

--- dsm2-grid-map/src/gov/ca/bdo/modeling/dsm2/map/client/display/MapControlPanel.java
+++ dsm2-grid-map/src/gov/ca/bdo/modeling/dsm2/map/client/display/MapControlPanel.java
@@ -1,13 +1,21 @@
 package gov.ca.bdo.modeling.dsm2.map.client.display;
 
+import gov.ca.bdo.modeling.dsm2.map.client.map.ChannelLineDataManager;
+import gov.ca.bdo.modeling.dsm2.map.client.map.ColorSchemePanel;
 import gov.ca.bdo.modeling.dsm2.map.client.map.ElementType;
+import gov.ca.dsm2.input.model.Channel;
+import gov.ca.dsm2.input.model.Channels;
 
 import java.util.HashMap;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.maps.client.overlay.PolyStyleOptions;
+import com.google.gwt.maps.client.overlay.Polyline;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Button;
@@ -30,6 +38,7 @@ public class MapControlPanel extends Composite {
 
 	interface MapControlPanelUiBinder extends UiBinder<Widget, MapControlPanel> {
 	}
+
 	@UiField
 	TabLayoutPanel tabLayoutPanel;
 	@UiField
@@ -62,10 +71,21 @@ public class MapControlPanel extends Composite {
 	ScrollPanel infoPanel;
 	@UiField
 	Button cancelEditButton;
+	@UiField
+	ListBox schemeTypeBox;
+	@UiField
+	ListBox colorTypeBox;
+	@UiField
+	ToggleButton colorizeButton;
+	@UiField
+	FlowPanel colorPanel;
 
 	private HashMap<String, Integer> mapTypeToId;
+	private DSM2GridMapDisplay display;
 
-	public MapControlPanel(boolean viewOnly) {
+	public MapControlPanel(boolean viewOnly,
+			DSM2GridMapDisplay dsm2GridMapDisplay) {
+		display = dsm2GridMapDisplay;
 		initWidget(uiBinder.createAndBindUi(this));
 		mapTypeToId = new HashMap<String, Integer>();
 		mapTypeToId.put("Node", ElementType.NODE);
@@ -73,11 +93,13 @@ public class MapControlPanel extends Composite {
 		mapTypeToId.put("Reservoir", ElementType.RESERVOIR);
 		mapTypeToId.put("Gate", ElementType.GATE);
 		mapTypeToId.put("XSection", ElementType.XSECTION);
-		mapTypeToId.put("Output", ElementType.OUTPUT);
-		mapTypeToId.put("Text", ElementType.TEXT);
-		mapTypeToId.put("KML", ElementType.KML);
+		/*
+		 * mapTypeToId.put("Output", ElementType.OUTPUT);
+		 * mapTypeToId.put("Text", ElementType.TEXT); mapTypeToId.put("KML",
+		 * ElementType.KML);
+		 */
 		String[] elementTypes = new String[] { "Node", "Channel", "Reservoir",
-				"Gate", "XSection", "Output", "Text", "KML" };
+				"Gate", "XSection", /* "Output", "Text", "KML" */};
 		for (String item : elementTypes) {
 			elementTypeBox.addItem(item);
 		}
@@ -100,6 +122,100 @@ public class MapControlPanel extends Composite {
 				infoPanel.clear();
 			}
 		});
+		// colorize widgets
+		schemeTypeBox.addItem("SEQUENTIAL");
+		schemeTypeBox.addItem("DIVERGING");
+		schemeTypeBox.addItem("QUALITATIVE");
+		colorizeButton.addClickHandler(new ClickHandler() {
+
+			public void onClick(ClickEvent event) {
+				colorizeChannels();
+			}
+		});
+		schemeTypeBox.addChangeHandler(new ChangeHandler() {
+
+			public void onChange(ChangeEvent event) {
+				colorizeChannels();
+			}
+		});
+		colorTypeBox.addItem("Mannings");
+		colorTypeBox.addItem("Dispersion");
+		colorTypeBox.addChangeHandler(new ChangeHandler() {
+
+			public void onChange(ChangeEvent event) {
+				colorizeChannels();
+			}
+		});
+
+	}
+
+	public void colorizeChannels() {
+		colorPanel.clear();
+		if (!colorizeButton.isDown()) {
+			ChannelLineDataManager channelManager = display.getMapPanel()
+					.getChannelManager();
+			for (Polyline line : channelManager.getPolylines()) {
+				line.setStrokeStyle(channelManager.getPolylineStyle());
+			}
+			return;
+		}
+		HashMap<String, Double> channelIdToValueMap = getManningsMap();
+		if (colorTypeBox.getItemText(colorTypeBox.getSelectedIndex()).equals(
+				"Dispersion")) {
+			channelIdToValueMap = getDispersionMap();
+		}
+		String selected = schemeTypeBox.getItemText(schemeTypeBox
+				.getSelectedIndex());
+		String[] colors = ColorRangeMapper.SEQUENTIAL_COLORS;
+		if (selected.equalsIgnoreCase("SEQUENTIAL")) {
+			colors = ColorRangeMapper.SEQUENTIAL_COLORS;
+		} else if (selected.equalsIgnoreCase("DIVERGING")) {
+			colors = ColorRangeMapper.DIVERGING_COLORS;
+		} else if (selected.equalsIgnoreCase("QUALITATIVE")) {
+			colors = ColorRangeMapper.QUALITATIVE_COLORS;
+		}
+		ColorRangeMapper colorMapper = new ColorRangeMapper(colors);
+		ChannelLineDataManager channelManager = display.getMapPanel()
+				.getChannelManager();
+		double max = Double.MIN_VALUE, min = Double.MAX_VALUE;
+		for (String id : channelIdToValueMap.keySet()) {
+			Double value = channelIdToValueMap.get(id);
+			max = Math.max(max, value);
+			min = Math.min(min, value);
+		}
+		// normalize with color array length
+		max = Math.ceil(max * colors.length) / colors.length;
+		min = Math.floor(min * colors.length) / colors.length;
+		colorPanel.add(new ColorSchemePanel(colors, max, min));
+		for (String id : channelIdToValueMap.keySet()) {
+			Double value = channelIdToValueMap.get(id);
+			String colorSpec = colorMapper.convertValueToColor(value, min, max);
+			PolyStyleOptions styleOptions = PolyStyleOptions
+					.newInstance(colorSpec);
+			styleOptions.setOpacity(1.0);
+			styleOptions.setWeight(3);
+			channelManager.getPolyline(id).setStrokeStyle(styleOptions);
+		}
+	}
+
+	public HashMap<String, Double> getManningsMap() {
+		Channels channels = display.getMapPanel().getChannelManager()
+				.getChannels();
+		HashMap<String, Double> map = new HashMap<String, Double>();
+		for (Channel c : channels.getChannels()) {
+			map.put(c.getId(), c.getMannings());
+		}
+		return map;
+	}
+
+	public HashMap<String, Double> getDispersionMap() {
+		Channels channels = display.getMapPanel().getChannelManager()
+				.getChannels();
+		HashMap<String, Double> map = new HashMap<String, Double>();
+		for (Channel c : channels.getChannels()) {
+			map.put(c.getId(), c.getDispersion());
+		}
+		return map;
 	}
 
 	public HasClickHandlers getAddButton() {
@@ -174,8 +290,8 @@ public class MapControlPanel extends Composite {
 	public HashMap<String, Integer> getMapTypeToId() {
 		return mapTypeToId;
 	}
-	
-	public void showEditPanel(){
+
+	public void showEditPanel() {
 		tabLayoutPanel.selectTab(0);
 	}
 
